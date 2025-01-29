2403be778d96587caf2ea2f99e165f776f4f1fe1
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2025-01-29 03:42:14 -0500

    Update dashboard styles and add game selection functionality

---
+++ controllers/Games/selectGames.js
@@ -0,0 +1,25 @@
+document.querySelector(".jsFilter").addEventListener("click", function () {
+  document.querySelector(".filter-menu").classList.toggle("active");
+});
+
+document.querySelector(".grid").addEventListener("click", function () {
+  document.querySelector(".list").classList.remove("active");
+  document.querySelector(".grid").classList.add("active");
+  document.querySelector(".games-area-wrapper").classList.add("gridView");
+  document
+    .querySelector(".games-area-wrapper")
+    .classList.remove("tableView");
+});
+
+document.querySelector(".list").addEventListener("click", function () {
+  document.querySelector(".list").classList.add("active");
+  document.querySelector(".grid").classList.remove("active");
+  document.querySelector(".games-area-wrapper").classList.remove("gridView");
+  document.querySelector(".games-area-wrapper").classList.add("tableView");
+});
+
+var modeSwitch = document.querySelector('.mode-switch');
+modeSwitch.addEventListener('click', function () {                      
+document.documentElement.classList.toggle('light');
+ modeSwitch.classList.toggle('active');
+});
